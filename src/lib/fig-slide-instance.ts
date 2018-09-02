9f91c2eecb3d37f387ac224cffb3abcbff26c553
Peter Burns <rictic@google.com>
2018-09-01 17:28:18 -0700

    More prerendering
    Also syntax highlight after first render.

--- src/lib/fig-slide-instance.ts
+++ src/lib/fig-slide-instance.ts
@@ -1,28 +1,28 @@
-import {LitElement} from '@polymer/lit-element';
-
-import {customElement} from './decorators.js';
-import {color} from '@justinfagnani/rainbow';
-
 // These imports register languages with rainbow
+import '@justinfagnani/rainbow/lib/language/python.js';
 import '@justinfagnani/rainbow/lib/language/html.js';
 import '@justinfagnani/rainbow/lib/language/css.js';
 import '@justinfagnani/rainbow/lib/language/javascript.js';
+
+import {color} from '@justinfagnani/rainbow';
+import {LitElement} from '@polymer/lit-element';
+
+import {customElement} from './decorators.js';
 
 /**
  * Used to create an empty ShadowRoot to render a slide into.
  */
 @customElement('fig-slide-instance')
 export abstract class FigSlideInstanceElement extends LitElement {
-
   // writing the update() override signature is a little annoying without
   // PropertyKey
   async update(changedProperties: Map<string, unknown>) {
     super.update(changedProperties);
-    console.log('before Color');
-    // await color(this.shadowRoot!);
-    console.log('after Color');
-    // const codeBlocks = this.shadowRoot!.querySelectorAll('pre');
-    // await Promise.all(Array.from(codeBlocks).map((b) => color(b)));
+    await color(this.shadowRoot!);
+  }
+
+  async firstRendered() {
+    await color(this.shadowRoot!);
   }
 }
 
